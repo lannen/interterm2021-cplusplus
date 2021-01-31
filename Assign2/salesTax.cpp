b2e51a2ee22fb40996f1604b07118521d63e9adf
root <annen@chapman.edu>
2021-01-31 06:00:18 +0000

    final updates

--- Assign2/salesTax.cpp
+++ Assign2/salesTax.cpp
@@ -9,14 +9,21 @@ Assignment 2 - sales tax
 #include <iostream>
 using namespace std;
 
-float addTax(float taxRate, float cost) {
-  float costValue;
+float addTax(float taxRate, float cost) { // calculates cost with tax
+  float finalCost;  // final cost
 
-  costValue = ((taxRate/100) * cost) + cost;
+  finalCost = ((taxRate/100) * cost) + cost;    // cost with tax
 
-  return costValue;
+  return finalCost;
 }
 
-int main() {
+int main(int argc, char **argv) {
+  // i know it has to be command line parameter
+  float taxRate = 7.75;
+  float cost = 23.45;
+
+  float result = addTax(taxRate, cost);
+  cout << result << endl;
+
   return 0;
 }
--- Assign2/zeroBoth.cpp
+++ Assign2/zeroBoth.cpp
@@ -9,11 +9,19 @@ Assignment 2 - zero both
 #include <iostream>
 using namespace std;
 
-void zeroBoth(int& int1, int& int2) {
+void zeroBoth(int& int1, int& int2) {   // sets values of reference parameters to 0
   int1 = 0;
   int2 = 0;
 }
 
-int main () {
+int main (int argc, char **argv) {
+  // i know it has to be command line parameter
+  int int1 = 1;
+  int int2 = 2;
+
+  zeroBoth(int1, int2);
+  cout << int1 << endl;
+  cout << int2 << endl;
+
   return 0;
 }
